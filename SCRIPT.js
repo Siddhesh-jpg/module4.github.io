0e78a590d02b5ac9d4505834cee0f79d1c4bcdf6
Siddhesh-jpg <68666911+Siddhesh-jpg@users.noreply.github.com>
2020-08-09 15:00:29 +0530

    Update SCRIPT.js

--- SCRIPT.js
+++ SCRIPT.js
@@ -3,17 +3,17 @@
 
 /*
 Output :
-
-Good bye jay
-Hello harsh
-Hello Mahesh
-Hello varun
-Good bye jaystri
-Hello metha
-Hello Siddhesh
-Good bye Jason
-Hello Parveen
+Hello Yaakov
 Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
 
 
 */
@@ -21,8 +21,11 @@ Good Bye John
 
 
 
-(function(){
-  var name=["jay","harsh","Mahesh","varun","jaystri","metha","Siddhesh","Jason","Parveen","John "];
+
+
+
+(function() {
+    var name = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim" ];
   for(var i=0;i<name.length;i++){
   var first=name[i].charAt(0).toLowerCase();  
     if(first=='j'){
